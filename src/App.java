d7ec5c81ea1bc8a6f652819cd3209f8ea8bd2101
lilloly <lilyhung55@gmail.com>
2023-09-30 14:42:56 -0700

    scanner

--- src/App.java
+++ src/App.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter pulse");
+        System.out.println("Enter ...");
 
-        int pulse = input.nextInt();
-        System.out.println("Input: " + pulse);
+        String response = input.nextLine();
+
+        System.out.println("Result: " + response);
+        input.close();
     }
 }
